bb1c775450acd8795a47012361c9150a0705af67
dten1986 <dten1986@mail.ru>
2017-12-08 11:42:54 +0200

    -

--- app-angularjs/form-handler.php
+++ app-angularjs/form-handler.php
@@ -2,20 +2,25 @@
 	$file = getcwd() . '/form-data.json';
 
 	if ($_SERVER['QUERY_STRING'] && strrpos($_SERVER['QUERY_STRING'], 'submit') !== false) {
+		$data = json_decode(file_get_contents($file));
 		
 		$req = explode('=', $_SERVER['QUERY_STRING']);
+		$item = json_decode(urldecode($req[1]));
 		
-		$data = urldecode($req[1]);
+		if ($item->index == -1)
+			$data[] = $item->item;
+		else
+			if (isset($item->item))
+				$data[$item->index] = $item->item;
+			else
+				unset($data[$item->index]);
 		
-	//	if (!file_exists($file))
+		$data = json_encode($data);
 		file_put_contents($file, $data);
-		
-	//	print_r(file_exists($file));
-		//echo 'req';
-		echo '{"status": "OK","info": "Form data saved!", "data": ' . $data . '}';
+		echo '{"status": "OK","info": "Form data saved!"}';
 	} else {
-		if (!file_exists($file))
-			file_put_contents($file, '{}');
+		if (!file_exists($file) || !filesize($file))
+			file_put_contents($file, '[]');
 		
 		echo file_get_contents($file);
 	}
--- app-angularjs/js/app.js
+++ app-angularjs/js/app.js
@@ -1,83 +1,31 @@
 'use strict';
 var myApp = angular.module('myApp', []);
 
-myApp.controller('form', ['$scope', function($scope) {
-	function _xhr(url, callback) {
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url);
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4 && xhr.responseText)
-				callback(JSON.parse(xhr.responseText));
-		}
-				
-		xhr.send();
+myApp.controller('list', ['$scope', '$http', function($scope, $http) {
+	function syncList(data, callback) {
+		var params = {
+			method: 'GET',
+			url: 'form-handler.php' + (data ? '?submit=' + JSON.stringify(data) : ''),
+		};
+
+		$http(params).then(function successCallback(resp) {
+			callback(resp);
+		}, function errorCallback(resp) {
+			console.log('Form handler error!');
+		});
 	}
 	
-	// Saved form data
-	_xhr('form-handler.php', function(resp) {
-		$scope.form_data = resp;
-		$scope.$apply();
-	})
-	
-	$scope.form_data = '';
-	$scope.fields = [
-		{
-			id: 1,
-			type: 'text',
-			required: true
-		},
-		{
-			id: 2,
-			type: 'textarea',
-		},
-		{
-			id: 3,
-			type: 'select',
-			options: [1, 2, 3, 4, 5],
-		},
-		{
-			id: 4,
-			type: 'checkbox',
-			checked: false,
-		}
-	];
-	
-	$scope.addNewField = function() {
-		if (!$scope.addNew)
-			$scope.addNew = true;
-		else
-			$scope.addNew = false;
-		
-		
+	var defaultFormVal = {
+		title: 'Add new item',
+		text: '',
+		textarea: '',
+		select: 'Option 1',
+		checkbox: false,
+		index: -1
 	};
-	
-	$scope.removeField = function(id) {
-		for (var i = 0; i < $scope.fields.length; i++) {
-			if ($scope.fields[i].id == id) {
-				$scope.fields.splice(i, 1);
-				break;
-			}
-		}
-	}
-	
-	$scope.$watch('selType', function(newVal, oldVal) {
-		if (newVal) {
-			$scope.addNew = false;
-			
-			var newField = {
-				'id': $scope.fields.length + 1,
-				'type': newVal,
-			};
-			
-			if (newVal == 'select')
-				newField.options = [1, 2, 3];
-			
-			if (newVal == 'checkbox')
-				newField.checked = false;
-			
-			$scope.fields.push(newField);
-		}
-	});
+
+	$scope.list = [];
+	$scope.formVisible = false;
 	
 	$scope.submit = function(form) {
 		$scope.submitted = true;
@@ -85,21 +33,60 @@ myApp.controller('form', ['$scope', function($scope) {
 		if (form.$invalid)
 			return;
 
-		var config = {};
+		var newItem = {
+			checkbox: $scope.formVal.checkbox,
+			text: $scope.formVal.text,
+			textarea: $scope.formVal.textarea,
+			select: $scope.formVal.select
+		};
 		
-		for (var key in form)
-			if (key.indexOf('$') < 0)
-				config[key] = '' + form[key].$viewValue;
+		if ($scope.formVal.index == -1)
+			$scope.list.push(newItem);
+		else
+			$scope.list[$scope.formVal.index] = newItem;
+		
+		var data = {
+			index: $scope.formVal.index,
+			item: newItem
+		};
 
-		_xhr('form-handler.php?submit=' + JSON.stringify(config), function(resp) {
-			if (resp.status == 'OK') {
-				$scope.messages = resp.info;
-				$scope.form_data = resp.data;
-				$scope.submitted = false;
-				$scope.$apply();
-			}
+		syncList(data, function(resp) {
+			$scope.messages = resp.info;
+			$scope.submitted = false;
+			$scope.formVisible = false;
+		//	$scope.$apply();
 		});
 	};
 	
+	$scope.openForm = function() {
+		$scope.formVal = defaultFormVal;
+		$scope.formVisible = true;
+	}
 	
+	$scope.closeForm = function() {
+		$scope.formVisible = false;
+	}
+	
+	$scope.editItem = function(index) {
+		$scope.formVal = {
+			title: 'Edit item ' + (index + 1),
+			text: $scope.list[index].text,
+			textarea: $scope.list[index].textarea,
+			select: $scope.list[index].select,
+			checkbox: $scope.list[index].checkbox,
+			index: index
+		};
+		$scope.formVisible = true;
+	}
+
+	$scope.removeItem = function(item) {
+		var index = $scope.list.indexOf(item);
+		$scope.list.splice(index, 1);
+		syncList({index: index}, function() {});
+	}
+	
+	// action
+	syncList(null, function(resp) {
+		$scope.list = resp.data;
+	});
 }]);
